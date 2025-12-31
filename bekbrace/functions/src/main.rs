95d186bc78b83ad74519c1fb9baa4c385d8a245e
Justin <justincrypto702@gmail.com>
2025-12-31 01:40:24 -0800

    Add insert input values lesson

--- bekbrace/functions/src/main.rs
+++ bekbrace/functions/src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
     hello_world();
+    tell_height(182);
 }
 
 //hoisting allows us to call functions from anywhere
 fn hello_world(){
 println!("Hello");
+}
+
+//You can insert input type values
+fn tell_height(height: u32){
+    println!("My height is {} cm.", height);
 }
