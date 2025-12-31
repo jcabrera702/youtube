d3ababcd0b91a2cb4a5e044249ae94bd2a1a9d90
Justin <justincrypto702@gmail.com>
2025-12-31 01:48:00 -0800

    Add multiple values into human_id function

--- bekbrace/functions/src/main.rs
+++ bekbrace/functions/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     hello_world();
     tell_height(182);
+    human_id("Justin", 37, 172.0);
 }
 
 //hoisting allows us to call functions from anywhere
@@ -11,4 +12,9 @@ println!("Hello");
 //You can insert input type values
 fn tell_height(height: u32){
     println!("My height is {} cm.", height);
+}
+
+//You can insert more than one value
+fn human_id(name: &str, age: u32, height: f32){
+    println!("Hello my name is {}, I am {} years old and my height is {} cm.", name, age, height);
 }
