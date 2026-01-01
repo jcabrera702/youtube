e46206b8640efaccc0f4e4ade70d8fd0dc60031b
Justin <justincrypto702@gmail.com>
2025-12-31 23:40:38 -0800

    Add example of an expression block
    for variable assignment returning in a rustacean way
    the last line does not need ; or return

--- bekbrace/functions/src/main.rs
+++ bekbrace/functions/src/main.rs
@@ -2,6 +2,12 @@ fn main() {
     hello_world();
     tell_height(182);
     human_id("Justin", 37, 172.0);
+    let _X: i32 = {
+        let price: i32 = 5;
+        let qty: i32 = 10;
+        price * qty 
+    };
+    println!("Result is: {}", _X);
 }
 
 //hoisting allows us to call functions from anywhere
@@ -18,3 +24,8 @@ fn tell_height(height: u32){
 fn human_id(name: &str, age: u32, height: f32){
     println!("Hello my name is {}, I am {} years old and my height is {} cm.", name, age, height);
 }
+
+//Expressions and Statements
+//Expression: Anything that returns a value
+//Statement: Anything that does not return a value
+//Expression examples: 5, true & false, add(3,4), if/else condition, ({code})
