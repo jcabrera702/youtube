2ba2ce166044e6a0c931a7423b42ac2cf1963232
Justin <justincrypto702@gmail.com>
2026-01-03 20:05:52 -0800

    Made x mutable and r immutable

--- bekbrace/borrowing-references/src/main.rs
+++ bekbrace/borrowing-references/src/main.rs
@@ -2,11 +2,18 @@
 // Create Reference by add "&"
 // -I- Immutable Reference
 fn main() {
-    let x: i32 = 5;
+    
     // let x become mutable that way we reference and x will not be dropped
     // & sign added to type and variable x
-    let r: &i32 = &x;
+    
+    //made x mutable
+    //changes in line 11 mut, line 12 &mut, &mut 
+    // Notice how x value has changed when cargo run
+    //Only 1 mutable references and many immutable references can be done
+    let mut x: i32 = 5;
+    let r: &mut i32 = &mut x;
+    *r +=1;
     println!("X is equal to {}", x);
-    println!("r is equal to {}", r);
+    
     
 }
